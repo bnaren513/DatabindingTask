94288e46bb7b0d3d167f706b8c4da1b9f7769bb5
NarendraAmz <narendra.biswa@amzurtech.com>
2020-06-26 22:21:02 +0530

    Initial Commit

---
+++ DatabindingTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatabindingTask
+//
+//  Created by Narendra Biswa on 26/06/20.
+//  Copyright © 2020 Narendra Biswa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
